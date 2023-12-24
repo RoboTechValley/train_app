e37744bed012170b05a0fad49e7cf8494f6df61f
Shahriar-H <shakisk23@gmail.com>
2023-12-24 11:35:51 +0600

    Soil

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import database from '@react-native-firebase/database';
+
+const App = () => {
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    database()
+    .ref('/')
+    .on('value', snapshot => {
+      console.log('User dat: ', snapshot.val());
+      setdata(snapshot.val())
+    });
+  }, []);
+  return (
+    <View style={{flex:1}}>
+      <Text style={{textAlign:'center', color:'#000',fontSize:19,fontWeight:'bold',paddingTop:50}}>Realtime Soil Data</Text>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.heading}>PH</Text>
+          <Text style={styles.value}>{data?.PH}</Text>
+        </View>
+        {/* <View style={styles.box}>
+          <Text style={styles.heading}>Moisture</Text>
+          <Text style={styles.value}>0</Text>
+        </View> */}
+        <View style={styles.box}>
+          <Text style={styles.heading}>Water Level</Text>
+          <Text style={styles.value}>{data?.water_level}</Text>
+        </View>
+    </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginTop: -20,
+    flex:1,
+  },
+  box: {
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    width:'45%',
+    borderRadius:4,
+    backgroundColor:'#fff'
+  },
+  heading: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  value: {
+    fontSize: 26,
+    marginTop: 5,
+    fontWeight:'bold'
+  },
+});
+
+export default App;
